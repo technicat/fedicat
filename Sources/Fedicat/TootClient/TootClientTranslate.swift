7670f37c4f9d6a91fd2156b71182185df040aab3
Philip Chu <technicat@users.noreply.github.com>
2024-02-10 20:23:18 -0700

    Create TootClientTranslate.swift

---
+++ Sources/Fedicat/TootClient/TootClientTranslate.swift
@@ -0,0 +1,21 @@
+import TootSDK
+
+extension TootClient {
+
+  public func getPostTranslation(id: String, language: String) async throws -> Translation {
+    try await getPostTranslation(
+      id: id,
+      params: PostTranslationParams(lang: language))
+  }
+
+  public func getTranslation(of post: Post, language: String) async throws -> Translation {
+    try await getPostTranslation(
+      id: post.id,
+      params: PostTranslationParams(lang: language))
+  }
+
+  public func getTranslation(of post: Post) async throws -> Translation {
+    try await getPostTranslation(
+      id: post.id)
+  }
+}
