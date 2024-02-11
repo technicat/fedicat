d7a73605a4913e59072bf143519501961a801996
Philip Chu <technicat@users.noreply.github.com>
2024-02-11 13:28:40 -0700

    Update TootClientAnnouncement.swift

--- Sources/Fedicat/TootClient/TootClientAnnouncement.swift
+++ Sources/Fedicat/TootClient/TootClientAnnouncement.swift
@@ -6,4 +6,12 @@ extension TootClient {
   public func delete(_ announce: Announcement) async throws {
     try await dismissAnnouncement(id: announce.id)
   }
+
+  public func react(to announce: Announcement, with reaction: AnnouncementReaction) async throws {
+    try await addAnnouncementReaction(id: announce.id, name: reaction.name)
+  }
+
+  public func unReact(to announce: Announcement, with reaction: AnnouncementReaction) async throws {
+    try await removeAnnouncementReaction(id: announce.id, name: reaction.name)
+  }
 }
