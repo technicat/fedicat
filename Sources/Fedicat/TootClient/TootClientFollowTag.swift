10565967bfec0834973c6a50ca9a176146896abb
Philip Chu <technicat@users.noreply.github.com>
2024-02-11 10:57:31 -0700

    format

--- Sources/Fedicat/TootClient/TootClientFollowTag.swift
+++ Sources/Fedicat/TootClient/TootClientFollowTag.swift
@@ -1,12 +1,12 @@
 import TootSDK
 
 extension TootClient {
-    
-    func follow(tag name: String) async throws -> Tag {
-      try await followTag(name)
-    }
-    
-    func unFollow(tag name: String) async throws -> Tag {
-      try await unfollowTag(name)
-    }
+
+  func follow(tag name: String) async throws -> Tag {
+    try await followTag(name)
+  }
+
+  func unFollow(tag name: String) async throws -> Tag {
+    try await unfollowTag(name)
+  }
 }
