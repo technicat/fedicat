0fdd7e86d40812dbbe8fa6abe999db0281587f2b
Philip Chu <technicat@users.noreply.github.com>
2024-02-11 14:00:03 -0700

    conversation

--- Sources/Fedicat/TootClient/TootClientAnnouncement.swift
+++ Sources/Fedicat/TootClient/TootClientAnnouncement.swift
@@ -2,7 +2,6 @@ import TootSDK
 
 extension TootClient {
 
-  // in practice, just acts as mark as read
   public func delete(_ announce: Announcement) async throws {
     try await dismissAnnouncement(id: announce.id)
   }
---
+++ Sources/Fedicat/TootClient/TootClientConversation.swift
@@ -0,0 +1,11 @@
+import TootSDK
+
+extension TootClient {
+    public func delete(_ conversation: Conversation) async throws {
+        try await deleteConversation(id: conversation.id)
+    }
+    
+    public func markRead(_ conversation: Conversation) async throws -> Conversation {
+        try await setConversationAsRead(id: conversation.id)
+    }
+}
