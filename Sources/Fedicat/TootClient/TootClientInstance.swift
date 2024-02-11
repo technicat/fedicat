1dc7d916e2a6625fceb57ed3e9b79f8444a8aebf
Philip Chu <technicat@users.noreply.github.com>
2024-02-10 20:38:44 -0700

    Update TootClientInstance.swift

--- Sources/Fedicat/TootClient/TootClientInstance.swift
+++ Sources/Fedicat/TootClient/TootClientInstance.swift
@@ -6,10 +6,6 @@ extension TootClient {
   /// ensures that client is updated with flavour
   public func getInstance() async throws -> Instance {
     let instance = try await getInstanceInfo()
-    if debugInstance {
-      Logger.log.info("flavour: \(instance.flavour.display)")
-      Logger.log.info("version: \(instance.version)")
-    }
     flavour = instance.flavour
     return instance
   }
