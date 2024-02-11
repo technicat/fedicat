6110c61914b373237436b2b6118a89e2926748a8
Philip Chu <technicat@users.noreply.github.com>
2024-02-11 01:04:53 -0700

    Update TootClientMuteAccount.swift

--- Sources/Fedicat/TootClient/TootClientMuteAccount.swift
+++ Sources/Fedicat/TootClient/TootClientMuteAccount.swift
@@ -1,17 +1,15 @@
 import TootSDK
 
 extension TootClient {
-    
-    public func mute(
-        _ account: Account,
-        params: MuteAccountParams? = nil
-    ) async throws -> Relationship {
-        try await muteAccount(
-            by: account.id,
-            params: params)
-    }
-    
-    public func unMute(_ account: Account) async throws -> Relationship {
-        try await unmuteAccount(by: account.id)
-    }
+
+  public func mute(
+    _ account: Account,
+    params: MuteAccountParams? = nil
+  ) async throws -> Relationship {
+    try await muteAccount(by: account.id, params: params)
+  }
+
+  public func unMute(_ account: Account) async throws -> Relationship {
+    try await unmuteAccount(by: account.id)
+  }
 }
