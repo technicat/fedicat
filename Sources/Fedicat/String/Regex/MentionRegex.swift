5a2cf1c24f26b04db4521f68e27fb88dc2901cf0
Philip Chu <technicat@users.noreply.github.com>
2024-02-11 17:41:20 -0700

    regex

---
+++ Sources/Fedicat/String/Regex/MentionRegex.swift
@@ -0,0 +1,25 @@
+import Foundation
+import RegexBuilder
+
+@available(macOS 13, *)
+@available(iOS 16, *)
+extension String {
+
+  public var bfMention: String {
+    replacing(String.mentionRegex, with: { match in "\(match.output.1)**\(match.output.2)**" })
+  }
+
+  private static let mentionRegex = Regex {
+    Capture {
+      ChoiceOf {
+        Anchor.startOfLine
+        One(.whitespace)
+      }
+    }
+    Capture {
+      "@"
+      OneOrMore(.word)
+    }
+  }
+
+}
