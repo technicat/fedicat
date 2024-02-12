aef40afb275610b6b6a9f1ddd96a789e0482dd47
Philip Chu <technicat@users.noreply.github.com>
2024-02-11 19:05:56 -0700

    Update PollVoted.swift

--- Sources/Fedicat/Poll/PollVoted.swift
+++ Sources/Fedicat/Poll/PollVoted.swift
@@ -4,4 +4,19 @@ extension Poll {
     public var hasVoted: Bool {
         voted ?? false
     }
+    
+    public func index(of option: Poll.Option) -> Int? {
+      options.firstIndex(of: option)
+    }
+    
+    public func didVote(for index: Int) -> Bool {
+      ownVotes?.contains(index) ?? false
+    }
+
+    public func didVote(for option: Poll.Option) -> Bool {
+        guard let index = index(of: option) else {
+            return false
+        }
+            return didVote(for: index)
+    }
 }
