09431764024e87ca3638dc222f421a042f452df5
Philip Chu <technicat@users.noreply.github.com>
2024-02-12 14:09:50 -0700

    fixes

--- Sources/Fedicat/TootClient/TootClientFeaturedTag.swift
+++ Sources/Fedicat/TootClient/TootClientFeaturedTag.swift
@@ -2,7 +2,7 @@ import TootSDK
 
 extension TootClient {
 
-  func getFeaturedTags(of account: Account) async throws -> [FeaturedTag] {
+  public func getFeaturedTags(of account: Account) async throws -> [FeaturedTag] {
     try await getFeaturedTags(forUser: account.id)
   }
 
--- Sources/Fedicat/TootClient/TootClientPoll.swift
+++ Sources/Fedicat/TootClient/TootClientPoll.swift
@@ -3,7 +3,7 @@ import TootSDK
 extension TootClient {
 
   public func refresh(_ poll: Poll) async throws -> Poll {
-    getPoll(id: poll.id)
+    try await getPoll(id: poll.id)
   }
 
 }
