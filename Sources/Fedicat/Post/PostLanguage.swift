87a92a21fee0683e6d2876f3173a5635ff257a64
Philip Chu <technicat@users.noreply.github.com>
2024-02-12 20:22:01 -0700

    Update PostLanguage.swift

--- Sources/Fedicat/Post/PostLanguage.swift
+++ Sources/Fedicat/Post/PostLanguage.swift
@@ -1,8 +1,16 @@
+import Foundation
 import TootSDK
 
 extension Post {
 
   public var hasLanguage: Bool {
     language != nil
+  }
+
+  public var languageCode: Locale.LanguageCode? {
+    guard let lang = language else {
+      return nil
+    }
+    return Locale.LanguageCode(lang)  // preprocess string?
   }
 }
