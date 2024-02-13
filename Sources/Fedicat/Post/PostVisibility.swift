b297b2ff56ab88dad439cecde0ee40c0ee0b8b19
Philip Chu <technicat@users.noreply.github.com>
2024-02-12 18:35:49 -0700

    Post Visibility

---
+++ Sources/Fedicat/Post/PostVisibility.swift
@@ -0,0 +1,16 @@
+import TootSDK
+
+extension Post {
+
+  public var isPublic: Bool {
+    visibility == .public
+  }
+
+  public var isUnlisted: Bool {
+    visibility == .unlisted
+  }
+
+  public var isVisible: Bool {
+    isPublic || isUnlisted
+  }
+}
