f90c7f21cd7358bddfabb7a89f3804590b9ab0c3
Philip Chu <technicat@users.noreply.github.com>
2024-02-14 01:15:51 -0700

    report categoreis

---
+++ Sources/Fedicat/Flav/FlavReportCategories.swift
@@ -0,0 +1,14 @@
+import TootSDK
+
+extension TootSDKFlavour {
+    public var reportCategories: [ReportCategory] {
+        switch self {
+        case .firefish:
+            return []
+        case .pixelfed:
+            return Array(ReportCategory.pixelfedSupported)
+        default:
+            return Array(ReportCategory.mastodonSupported)
+        }
+    }
+}
