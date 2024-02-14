d873c4078e6cdaa8cbadd00ce0cda3554e0d90bb
Philip Chu <technicat@users.noreply.github.com>
2024-02-14 00:23:35 -0700

    Update TagFeature.swift

--- Sources/Fedicat/Tag/TagFeature.swift
+++ Sources/Fedicat/Tag/TagFeature.swift
@@ -1,15 +1,15 @@
 import TootSDK
 
 extension TootClient {
-  func feature(_ tag: Tag) async throws -> FeaturedTag {
+  public func feature(_ tag: Tag) async throws -> FeaturedTag {
     try await feature(tag: tag.name)
   }
 
-  func unFeature(_ tag: FeaturedTag) async throws {
+  public func unFeature(_ tag: FeaturedTag) async throws {
     try await unfeatureTag(id: tag.id)
   }
 
-  func feature(tag name: String) async throws -> FeaturedTag {
+  public func feature(tag name: String) async throws -> FeaturedTag {
     return try await featureTag(name: name)
   }
 }
