789a32c3cb97ef1c4b46df2d15e18630c411596c
Philip Chu <technicat@users.noreply.github.com>
2024-02-13 17:40:05 -0700

    doc for visible

--- Sources/Fedicat/Post/PostVisibility.swift
+++ Sources/Fedicat/Post/PostVisibility.swift
@@ -2,14 +2,18 @@ import TootSDK
 
 extension Post {
 
+  /// totally public
   public var isPublic: Bool {
     visibility == .public
   }
 
+  /// public but not in timelines
   public var isUnlisted: Bool {
     visibility == .unlisted
   }
 
+  /// publicly accessible, i.e. not followers-only or more limited
+  /// required for boosting, and translation
   public var isVisible: Bool {
     isPublic || isUnlisted
   }
