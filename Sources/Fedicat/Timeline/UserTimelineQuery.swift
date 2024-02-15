1f498def7615c4453b4b9504d20bf7c309ad3a1d
Philip Chu <technicat@users.noreply.github.com>
2024-02-15 00:35:36 -0700

    Update UserTimelineQuery.swift

--- Sources/Fedicat/Timeline/UserTimelineQuery.swift
+++ Sources/Fedicat/Timeline/UserTimelineQuery.swift
@@ -2,7 +2,7 @@ import TootSDK
 
 extension UserTimelineQuery {
 
-  public init(for account: Account, onlyMedia: Bool? = nil) {
-      self.init(userId: account.id, onlyMedia: onlyMedia)
+  public init(for account: Account, onlyMedia: Bool? = nil, onlyPins pinned: Bool? = nil) {
+      self.init(userId: account.id, onlyMedia: onlyMedia, pinned: pinned)
   }
 }
