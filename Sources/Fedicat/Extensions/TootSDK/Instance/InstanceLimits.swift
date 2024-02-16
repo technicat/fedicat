7c9c1f2704be256681c80fe08382524031006cb5
Philip Chu <technicat@users.noreply.github.com>
2024-02-15 19:44:16 -0700

    Create InstanceLimits.swift

---
+++ Sources/Fedicat/Extensions/TootSDK/Instance/InstanceLimits.swift
@@ -0,0 +1,16 @@
+import TootSDK
+
+extension Instance {
+    
+    public var maxPostChars: Int? {
+        configuration?.posts?.maxCharacters
+    }
+    
+    public var maxAttachments: Int? {
+        configuration?.posts?.maxMediaAttachments
+    }
+    
+    public var maxFeaturedTags: Int? {
+      configuration?.accounts?.maxFeaturedTags
+    }
+}
