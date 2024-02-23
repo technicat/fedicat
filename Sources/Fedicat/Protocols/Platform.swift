e3225a78f3392821778ead73aba2e6249cbeb3c6
Philip Chu <technicat@users.noreply.github.com>
2024-02-23 11:31:55 -0700

    Create Platform.swift

---
+++ Sources/Fedicat/Protocols/Platform.swift
@@ -0,0 +1,42 @@
+import TootSDK
+
+public protocol Platform {
+
+  var name: String { get }
+  var languages: [ISOCode] { get }
+  var supportsTranslate: Bool { get }
+  var postVis: [Post.Visibility] { get }
+  var reportCats: [ReportCategory] { get }
+
+  var supportsProfileFields: Bool { get }
+  var supportsAnnouncements: Bool { get }
+  var supportsAnnouncementMark: Bool { get }
+  var supportsBookmark: Bool { get }
+  var supportsMutePost: Bool { get }
+  var supportsIsBoosted: Bool { get }
+  var supportsTrendingPosts: Bool { get }
+  var supportsTrendingTags: Bool { get }
+  var supportsTrendingLinks: Bool { get }
+  var supportsFollowTag: Bool { get }
+  var supportsNotificationTypes: Bool { get }
+  var supportsNotificationDelete: Bool { get }
+  var supportsList: Bool { get }
+  var supportsTagStats: Bool { get }
+  var supportsSearchAccounts: Bool { get }
+  var supportsSearchPosts: Bool { get }
+  var supportsPostLanguages: Bool { get }
+  var supportsFollowLanguages: Bool { get }
+  var supportsSchedule: Bool { get }
+  var supportsFamiliarFollowers: Bool { get }
+  var supportsFaveTimeline: Bool { get }
+  var supportsFilter: Bool { get }
+  var supportsProfileDirectory: Bool { get }
+  var supportsReportRules: Bool { get }
+  var supportsDomainBlocks: Bool { get }
+  var supportsRemoveFollower: Bool { get }
+  var supportsExtendedDescription: Bool { get }
+  var supportsProfileHeader: Bool { get }
+  var supportsPublicTimeline: Bool { get }
+  var supportsUpdateAccount: Bool { get }
+
+}
