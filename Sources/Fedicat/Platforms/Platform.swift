a05039e3216f34bb51643c6fc325396445a7bf06
Philip Chu <technicat@users.noreply.github.com>
2024-02-24 00:23:43 -0700

    Update Platform.swift

--- Sources/Fedicat/Platforms/Platform.swift
+++ Sources/Fedicat/Platforms/Platform.swift
@@ -6,37 +6,52 @@ public protocol Platform {
   var languages: [ISOCode] { get }
   var postVis: [Post.Visibility] { get }
   var reportCats: [ReportCategory] { get }
-  var supportsTranslate: Bool { get }
-  var supportsTranslationLanguages: Bool { get }
-  var supportsProfileFields: Bool { get }
+
   var supportsAnnouncements: Bool { get }
   var supportsAnnouncementMark: Bool { get }
+
   var supportsBookmark: Bool { get }
-  var supportsMutePost: Bool { get }
-  var supportsIsBoosted: Bool { get }
-  var supportsTrendingPosts: Bool { get }
-  var supportsTrendingTags: Bool { get }
-  var supportsTrendingLinks: Bool { get }
-  var supportsFollowTag: Bool { get }
-  var supportsNotificationTypes: Bool { get }
-  var supportsNotificationDelete: Bool { get }
-  var supportsNotificationDeleteAll: Bool { get }
-  var supportsList: Bool { get }
-  var supportsTagStats: Bool { get }
-  var supportsSearchAccounts: Bool { get }
-  var supportsSearchPosts: Bool { get }
-  var supportsFollowLanguages: Bool { get }
-  var supportsSchedule: Bool { get }
+  var supportsDomainBlocks: Bool { get }
+
+  var supportsExtendedDescription: Bool { get }
+
   var supportsFamiliarFollowers: Bool { get }
   var supportsFaveTimeline: Bool { get }
   var supportsFilter: Bool { get }
+
+  var supportsFollowLanguages: Bool { get }
+  var supportsFollowTag: Bool { get }
+
+  var supportsIsBoosted: Bool { get }
+
+  var supportsList: Bool { get }
+
+  var supportsMutePost: Bool { get }
+
+  var supportsNotificationDelete: Bool { get }
+  var supportsNotificationDeleteAll: Bool { get }
+  var supportsNotificationTypes: Bool { get }
+
+  var supportsPollVote: Bool { get }
+  var supportsProfileFields: Bool { get }
   var supportsProfileDirectory: Bool { get }
-  var supportsReportRules: Bool { get }
-  var supportsDomainBlocks: Bool { get }
-  var supportsRemoveFollower: Bool { get }
-  var supportsExtendedDescription: Bool { get }
   var supportsProfileHeader: Bool { get }
   var supportsPublicTimeline: Bool { get }
+
+  var supportsRemoveFollower: Bool { get }
+  var supportsReportRules: Bool { get }
+
+  var supportsSchedule: Bool { get }
+  var supportsSearchAccounts: Bool { get }
+  var supportsSearchPosts: Bool { get }
+
+  var supportsTagStats: Bool { get }
+  var supportsTranslate: Bool { get }
+  var supportsTranslationLanguages: Bool { get }
+  var supportsTrendingPosts: Bool { get }
+  var supportsTrendingTags: Bool { get }
+  var supportsTrendingLinks: Bool { get }
+
   var supportsUpdateAccount: Bool { get }
 
 }
