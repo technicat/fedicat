f0648c69d7c3be9e1533450432c21a8aaa8b8b19
Philip Chu <technicat@users.noreply.github.com>
2024-02-25 00:39:29 -0700

    Update Snac.swift

--- Sources/Fedicat/Platforms/Snac.swift
+++ Sources/Fedicat/Platforms/Snac.swift
@@ -1,3 +1,5 @@
 open class Snac: MastoAPI {
   open override var name: String { "Hometown" }
+  open override var supportsExtendedDescription: Bool { false }
+  open override var supportsTranslate: Bool { false }
 }
