502e2e7a7dde1140664e1e61828cf25f849ba355
Philip Chu <technicat@users.noreply.github.com>
2024-02-24 19:12:51 -0700

    Create PIxelfed.swift

---
+++ Sources/Fedicat/Platforms/PIxelfed.swift
@@ -0,0 +1,30 @@
+import TootSDK
+
+open class Pixelfed: MastoAPI {
+
+  open override var name: String { "Pixelfed" }
+  open override var supportsProfileFields: Bool { false }
+  open override var supportsFollowTag: Bool { true }
+  open override var supportsNotificationDelete: Bool { false }
+  open override var supportsNotificationDeleteAll: Bool { false }
+  open override var supportsNotificationTypes: Bool { false }
+  open override var supportsList: Bool { false }
+  open override var supportsSearchAccounts: Bool { false }
+  open override var supportsSearchPosts: Bool { false }
+  open override var supportsProfileHeader: Bool { false }
+  open override var supportsPublicTimeline: Bool { false }
+  open override var reportCats: [ReportCategory] {
+    [
+      .spam,
+      .sensitive,
+      .abusive,
+      .underage,
+      .violence,
+      .copyright,
+      .impersonation,
+      .scam,
+      .terrorism,
+    ]
+  }
+
+}
