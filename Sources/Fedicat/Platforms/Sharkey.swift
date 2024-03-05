253582bbe203866db7674fa438d9ef34aa2fcbd7
Philip Chu <technicat@users.noreply.github.com>
2024-03-04 20:30:23 -0700

    rm post defaults from sharkey

--- Sources/Fedicat/Platforms/Sharkey.swift
+++ Sources/Fedicat/Platforms/Sharkey.swift
@@ -12,6 +12,10 @@ open class Sharkey: MastoAPI {
   open override var supportsNotificationDeleteAll: Bool { false }
   open override var supportsNotificationTypes: Bool { false }
 
+  open override var supportsPostDefaultLanguage: Bool { false }
+  open override var supportsPostDefaultSensitive: Bool { false }
+  open override var supportsPostDefaultVisibility: Bool { false }
+
   open override var supportsTagStats: Bool { false }
   open override var supportsTagTimeline: Bool { false }  // bug
 }
