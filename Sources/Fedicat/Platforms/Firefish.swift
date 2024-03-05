628c05cb712a74fa2100258d393db3a2861065f3
Philip Chu <technicat@users.noreply.github.com>
2024-03-04 20:47:55 -0700

    post defaults

--- Sources/Fedicat/Platforms/Firefish.swift
+++ Sources/Fedicat/Platforms/Firefish.swift
@@ -3,13 +3,22 @@ import TootSDK
 open class Firefish: MastoAPI {
 
   open override var name: String { "Firefish" }
+
   open override var reportCats: [ReportCategory] { [] }
+
   open override var supportsDomainBlocks: Bool { false }
+
   open override var supportsNotificationDelete: Bool { false }
   open override var supportsNotificationDeleteAll: Bool { false }
   open override var supportsNotificationTypes: Bool { false }
+
+  open override var supportsPostDefaultLanguage: Bool { false }
+  open override var supportsPostDefaultSensitive: Bool { false }
+  open override var supportsPostDefaultVisibility: Bool { false }
   open override var supportsPublicTimeline: Bool { false }
+
   open override var supportsSearchAccounts: Bool { false }
+
   open override var supportsTranslate: Bool { false }
   open override var supportsTranslationLanguages: Bool { false }
   open override var supportsTrendingTags: Bool { false }
--- Sources/Fedicat/Platforms/Pleroma.swift
+++ Sources/Fedicat/Platforms/Pleroma.swift
@@ -2,4 +2,7 @@ open class Pleroma: MastoAPI {
 
   open override var name: String { "Pleroma" }
   open override var supportsNotificationTypes: Bool { false }
+
+  open override var supportsPostDefaultLanguage: Bool { false }
+  open override var supportsPostDefaultSensitive: Bool { false }
 }
