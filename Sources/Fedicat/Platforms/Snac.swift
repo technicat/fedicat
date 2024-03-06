10125843a3449a8b3669b8a5185d5eea4209d22e
Philip Chu <technicat@users.noreply.github.com>
2024-03-05 23:46:38 -0700

    fix snac name

--- Sources/Fedicat/Platforms/Snac.swift
+++ Sources/Fedicat/Platforms/Snac.swift
@@ -1,5 +1,5 @@
 open class Snac: MastoAPI {
-  open override var name: String { "Hometown" }
+  open override var name: String { "Snac" }
   open override var supportsExtendedDescription: Bool { false }
   open override var supportsTranslate: Bool { false }
 }
