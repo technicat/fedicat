24a779b099f14bb821e6427be8e09d9426cacf3c
Philip Chu <technicat@users.noreply.github.com>
2024-03-12 22:55:09 -0600

    Update TootClientAccount.swift

--- Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientAccount.swift
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientAccount.swift
@@ -5,4 +5,20 @@ extension TootClient {
   public func refresh(_ account: Account) async throws -> Account {
     try await getAccount(by: account.id)
   }
+
+  public func updateAccountSettings(
+    locked: Bool? = nil,
+    bot: Bool? = nil,
+    discoverable: Bool? = nil,
+    hideCollections: Bool? = nil,
+    indexable: Bool? = nil
+  ) async throws -> Account {
+    let params = UpdateCredentialsParams(
+      locked: locked,
+      bot: bot,
+      discoverable: discoverable,
+      hideCollections: hideCollections,
+      indexable: indexable)
+    return try await updateCredentials(params: params)
+  }
 }
