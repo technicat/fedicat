8da2070a2f955e4d88278694b56f68e70b19ba8b
Philip Chu <technicat@users.noreply.github.com>
2024-03-16 22:59:57 -0600

    Update TootClientNotificationDelete.swift

--- Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientNotificationDelete.swift
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientNotificationDelete.swift
@@ -6,4 +6,9 @@ extension TootClient {
   public func delete(_ notification: TootNotification) async throws {
     try await dismissNotification(id: notification.id)
   }
+
+  /// https://docs.joinmastodon.org/methods/notifications/#clear
+  public func deleteNotifications() async throws {
+    try await dismissAllNotifications()
+  }
 }
