b3a7364d8f3e4333eabac248cf70c1c839aa992c
Philip Chu <technicat@users.noreply.github.com>
2024-03-16 19:10:36 -0600

    Update TootClientFilterRefresh.swift

--- Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientFilterRefresh.swift
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientFilterRefresh.swift
@@ -1,8 +1,8 @@
 import TootSDK
 
 extension TootClient {
-    
-    public func refresh(_ filter: Filter) async throws -> Filter {
-        try await getFilter(id: filter.id)
-    }
+
+  public func refresh(_ filter: Filter) async throws -> Filter {
+    try await getFilter(id: filter.id)
+  }
 }
