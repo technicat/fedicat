d1cc6690539067636ef8169a35a34173ad9765d0
Philip Chu <technicat@users.noreply.github.com>
2024-03-18 01:11:26 -0600

    Update TootClientAccountMute.swift

--- Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientAccountMute.swift
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientAccountMute.swift
@@ -5,9 +5,22 @@ extension TootClient {
   /// https://docs.joinmastodon.org/methods/accounts/#mute
   public func mute(
     _ account: Account,
-    params: MuteAccountParams? = nil
+    params: MuteAccountParams
   ) async throws -> Relationship {
     try await muteAccount(by: account.id, params: params)
+  }
+
+  public func mute(
+    _ account: Account,
+    notifications: Bool = true,
+    duration: Int = 0
+  ) async throws -> Relationship {
+    let params = MuteAccountParams(
+      notifications: notifications,
+      duration: duration)
+    return try await mute(
+      account,
+      params: params)
   }
 
   /// https://docs.joinmastodon.org/methods/accounts/#unmute
