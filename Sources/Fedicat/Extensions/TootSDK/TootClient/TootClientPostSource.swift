ebfd76860c4f3467abb11a19ef1fcde787197b58
Philip Chu <technicat@users.noreply.github.com>
2024-03-20 10:42:39 -0600

    Update TootClientPostSource.swift

--- Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientPostSource.swift
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientPostSource.swift
@@ -2,6 +2,7 @@ import TootSDK
 
 extension TootClient {
 
+  /// https://docs.joinmastodon.org/methods/statuses/#source
   public func getSource(of post: Post) async throws -> PostSource {
     try await getPostSource(id: post.id)
   }
