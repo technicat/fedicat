df5f84e51bf5cba1e2fcad745d946a9096f0a893
Philip Chu <technicat@users.noreply.github.com>
2024-03-20 10:27:58 -0600

    api links for poset fave

--- Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientPostFave.swift
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientPostFave.swift
@@ -2,14 +2,17 @@ import TootSDK
 
 extension TootClient {
 
+  /// https://docs.joinmastodon.org/methods/statuses/#favourite
   public func fave(_ post: Post) async throws -> Post {
     try await favouritePost(id: post.id)
   }
 
+  /// https://docs.joinmastodon.org/methods/statuses/#unfavourite
   public func unFave(_ post: Post) async throws -> Post {
     try await unfavouritePost(id: post.id)
   }
 
+  /// https://docs.joinmastodon.org/methods/statuses/#favourited_by
   public func getFavers(
     of post: Post,
     _ page: PagedInfo? = nil,
