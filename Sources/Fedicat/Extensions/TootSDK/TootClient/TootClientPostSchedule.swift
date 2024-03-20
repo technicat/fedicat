872008124f3022cb4096345e3356348c2eedd0c8
Philip Chu <technicat@users.noreply.github.com>
2024-03-20 01:02:41 -0600

    Create TootClientPostSchedule.swift

---
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientPostSchedule.swift
@@ -0,0 +1,30 @@
+import Foundation
+import TootSDK
+
+extension TootClient {
+
+    /// todo - handle poll
+    /// todo - handle pleroma params (contentType and replyToConversationId)
+  public func schedule(
+    text: String,
+    mediaIds: [String]? = nil,
+    inReplyToId: String? = nil,
+    sensitive: Bool? = nil,
+    spoilerText: String? = nil,
+    visibility: Post.Visibility,
+    language: ISOCode,
+    scheduledAt: Date
+  ) async throws -> ScheduledPost {
+    try await schedulePost(
+      ScheduledPostParams(
+        text: text,
+        mediaIds: mediaIds,
+        sensitive: sensitive,
+        spoilerText: spoilerText,
+        visibility: visibility,
+        language: language.rawValue,
+        scheduledAt: scheduledAt,
+        inReplyToId: inReplyToId
+      ))
+  }
+}
