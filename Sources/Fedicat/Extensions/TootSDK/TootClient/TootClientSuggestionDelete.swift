02941b166702eae83dc0394e2df22ed8d94f9b93
Philip Chu <technicat@users.noreply.github.com>
2024-03-20 11:36:12 -0600

    Update TootClientSuggestionDelete.swift

--- Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientSuggestionDelete.swift
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientSuggestionDelete.swift
@@ -3,6 +3,7 @@ import TootSDK
 extension TootClient {
 
   // only works with source.past_interactions
+  /// https://docs.joinmastodon.org/methods/suggestions/#remove
   public func delete(_ suggestion: Suggestion) async throws {
     try await removeSuggestion(id: suggestion.account.id)
   }
