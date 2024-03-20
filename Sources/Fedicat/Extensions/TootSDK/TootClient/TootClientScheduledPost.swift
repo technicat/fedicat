d8cacf0634a17128dbce0da5fc7d7b67e7299606
Philip Chu <technicat@users.noreply.github.com>
2024-03-20 11:27:36 -0600

    api links

--- Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientScheduledPost.swift
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientScheduledPost.swift
@@ -1,11 +1,17 @@
 import TootSDK
 
 extension TootClient {
+    
+    /// https://docs.joinmastodon.org/methods/scheduled_statuses/#get-one
   public func refresh(_ post: ScheduledPost) async throws -> ScheduledPost? {
     try await getScheduledPost(id: post.id)
   }
 
+    /// https://docs.joinmastodon.org/methods/scheduled_statuses/#cancel
   public func delete(_ post: ScheduledPost) async throws {
     try await deleteScheduledPost(id: post.id)
   }
+    
+    /// todo
+    /// https://docs.joinmastodon.org/methods/scheduled_statuses/#update
 }
