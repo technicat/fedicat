4502293522c34a60d676519f3bc7023065ceaebb
Philip Chu <technicat@users.noreply.github.com>
2024-03-20 11:30:06 -0600

    api links

--- Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientTag.swift
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientTag.swift
@@ -2,6 +2,7 @@ import TootSDK
 
 extension TootClient {
 
+  /// https://docs.joinmastodon.org/methods/tags/#get
   public func refresh(_ tag: Tag) async throws -> Tag {
     try await getTag(tag.name)
   }
