092547bf90fb648854781182c86677c633a36a36
Philip Chu <technicat@users.noreply.github.com>
2024-03-20 10:57:12 -0600

    Update TootClientProfileDirectory.swift

--- Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientProfileDirectory.swift
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientProfileDirectory.swift
@@ -1,42 +1,28 @@
 import TootSDK
 
-/// https://docs.joinmastodon.org/methods/directory/
 extension TootClient {
 
-  public func getProfileDirectory(
-    order: ProfileDirectoryParams.Order? = nil,
-    local: Bool? = nil,
-    offset: Int? = nil,
-    limit: Int? = nil
-  ) async throws -> [Account] {
-    try await getProfileDirectory(
-      params: ProfileDirectoryParams(order: order, local: local),
-      offset: offset,
-      limit: limit
-    )
-  }
-
+  /// https://docs.joinmastodon.org/methods/directory/
   public func getLocalDirectory(
     order: ProfileDirectoryParams.Order? = nil,
     offset: Int? = nil,
     limit: Int? = nil
   ) async throws -> [Account] {
     try await getProfileDirectory(
-      order: order,
-      local: true,
+      params: ProfileDirectoryParams(order: order, local: true),
       offset: offset,
       limit: limit
     )
   }
 
+  /// https://docs.joinmastodon.org/methods/directory/
   public func getGlobalDirectory(
     order: ProfileDirectoryParams.Order? = nil,
     offset: Int? = nil,
     limit: Int? = nil
   ) async throws -> [Account] {
     try await getProfileDirectory(
-      order: order,
-      local: false,
+      params: ProfileDirectoryParams(order: order, local: false),
       offset: offset,
       limit: limit
     )
