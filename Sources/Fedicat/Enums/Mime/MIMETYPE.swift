95da930eeb93179da4161b01a03ef82c5dd2999a
Philip Chu <technicat@users.noreply.github.com>
2024-03-22 15:22:33 -0600

    mime

---
+++ Sources/Fedicat/Enums/Mime/MIMETYPE.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+// should just be for images, e.g. ImageMIME?
+public enum MIMEType: String, CaseIterable, Codable {
+    // image mime types supported by mastodon for attachments
+    // (media_attachment.rb)
+    case imageAVIF = "image/avif"
+    case imageGIF = "image/gif"
+    case imageHEIC = "image/heic"
+    case imageHEIF = "image/heif"
+    case imageJPEG = "image/jpeg"
+    case imageWebP = "image/webp"
+}
