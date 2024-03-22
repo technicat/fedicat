8c8216311b7b3ab2844391e7f532989af2282874
Philip Chu <technicat@users.noreply.github.com>
2024-03-22 15:29:39 -0600

    upload attachment

--- Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientMediaAttachment.swift
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientMediaAttachment.swift
@@ -1,11 +1,27 @@
+import Foundation
 import TootSDK
 
 extension TootClient {
-    public func getMedia(from media: UploadedMediaAttachment) async throws -> MediaAttachment? {
-        try await getMedia(id: media.id)
-    }
-    
-    public func refresh(_ media: MediaAttachment) async throws -> MediaAttachment? {
-        try await getMedia(id: media.id)
-    }
+  public func getMedia(from media: UploadedMediaAttachment) async throws -> MediaAttachment? {
+    try await getMedia(id: media.id)
+  }
+
+  public func refresh(_ media: MediaAttachment) async throws -> MediaAttachment? {
+    try await getMedia(id: media.id)
+  }
+
+  // todo - pass in thumbnail and focus
+  public func upload(
+    _ data: Data,
+    type: MIMEType,
+    description: String? = nil
+  ) async throws -> UploadedMediaAttachment {
+    try await uploadMedia(
+      UploadMediaAttachmentParams(
+        file: data,
+        thumbnail: nil,
+        description: description,
+        focus: nil),
+      mimeType: type.rawValue)
+  }
 }
