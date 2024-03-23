507a8982040932a56252be71433f48f1421af46d
Philip Chu <technicat@users.noreply.github.com>
2024-03-22 22:22:28 -0600

    added rules to reports

--- Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientReport.swift
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientReport.swift
@@ -1,22 +1,23 @@
 import TootSDK
 
 extension TootClient {
-    
-    // todo - pass ruleIds
+
     // firefish doesn't use category or ruleIds
     public func report(
         account: Account,
         comment: String?,
         forward: Bool?,
         category: ReportCategory,
-        posts: [Post]
+        posts: [Post] = [],
+        rules: [InstanceRule] = []
     ) async throws {
         let params = ReportParams(
             accountId: account.id,
             category: category,
             postIds: posts.map { $0.id },
             comment: comment,
-            forward: forward)
+            forward: forward,
+            ruleIds: rules.map { Int($0.id) ?? 0 })
         try await report(params)
     }
 }
