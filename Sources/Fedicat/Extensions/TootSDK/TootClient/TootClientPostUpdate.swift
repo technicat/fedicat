7c274b69f2d06eb7ad8d52d610c21f25acf05f3e
Philip Chu <technicat@users.noreply.github.com>
2024-03-23 21:02:19 -0600

    include language in post update

--- Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientPostUpdate.swift
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientPostUpdate.swift
@@ -2,18 +2,18 @@ import TootSDK
 
 extension TootClient {
 
-  /// todo - add language (4.0.0)
   /// todo - add mediaAttributes
   /// todo - add poll
   /// https://docs.joinmastodon.org/methods/statuses/#edit
   public func update(
-    _ post: Post, text: String, spoiler: String? = nil, sensitive: Bool? = nil,
+    _ post: Post, text: String, spoiler: String? = nil, sensitive: Bool? = nil, language: String? = nil,
     media: [MediaAttachment]? = nil, poll: CreatePoll? = nil
   ) async throws -> Post {
     let params = EditPostParams(
       post: text,
       spoilerText: spoiler,
       sensitive: sensitive,
+      language: language,
       mediaIds: media?.map { $0.id },
       poll: poll)
     return try await editPost(id: post.id, params)
