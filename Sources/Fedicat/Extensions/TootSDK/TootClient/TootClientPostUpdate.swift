a3c31b408cb7eb084b50d459ccefb2ef3e74c6ef
Philip Chu <technicat@users.noreply.github.com>
2024-03-23 23:36:35 -0600

    format

--- Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientPostUpdate.swift
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientPostUpdate.swift
@@ -6,7 +6,8 @@ extension TootClient {
   /// todo - add poll
   /// https://docs.joinmastodon.org/methods/statuses/#edit
   public func update(
-    _ post: Post, text: String, spoiler: String? = nil, sensitive: Bool? = nil, language: String? = nil,
+    _ post: Post, text: String, spoiler: String? = nil, sensitive: Bool? = nil,
+    language: String? = nil,
     media: [MediaAttachment]? = nil, poll: CreatePoll? = nil
   ) async throws -> Post {
     let params = EditPostParams(
