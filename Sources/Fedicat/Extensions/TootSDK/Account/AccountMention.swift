3db6737c255aa0763095d5c583205d7943b18205
Philip Chu <technicat@users.noreply.github.com>
2024-03-24 15:52:39 -0600

    Create AccountMention.swift

---
+++ Sources/Fedicat/Extensions/TootSDK/Account/AccountMention.swift
@@ -0,0 +1,16 @@
+import TootSDK
+
+extension Account {
+
+  public func isMentioned(by mention: Mention) -> Bool {
+    id == mention.id
+  }
+
+  public func isMentioned(by post: Post) -> Bool {
+    post.mentions.contains(where: { $0.id == id })
+  }
+
+  public func isSame(as account: Account) -> Bool {
+    id == account.id
+  }
+}
