88ec1f6f20632499aece1113edb2a79b754a4d43
Philip Chu <technicat@users.noreply.github.com>
2024-04-05 22:50:42 -0600

    pass withSuspended to getRelationship

--- Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientRelationship.swift
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientRelationship.swift
@@ -1,18 +1,19 @@
 import TootSDK
 
-// todo - pass with_suspended
 // https://docs.joinmastodon.org/methods/accounts/#relationships
 extension TootClient {
 
   public func getRelationship(with account: Account) async throws -> Relationship? {
-    let rships = try await getRelationships(with: [account])
+    let rships = try await getRelationships(with: [account], includeSuspended: true)
     guard !rships.isEmpty else {  // should check == 1?
       return nil
     }
     return rships.first
   }
 
-  public func getRelationships(with accounts: [Account]) async throws -> [Relationship] {
-    try await getRelationships(by: accounts.map { $0.id })
+  public func getRelationships(with accounts: [Account], includeSuspended: Bool = false)
+    async throws -> [Relationship]
+  {
+      try await getRelationships(by: accounts.map { $0.id }, params: RelationshipParams(withSuspended: includeSuspended))
   }
 }
