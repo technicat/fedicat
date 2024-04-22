6521db714ee5fec15624442e82558498db974cc9
Philip Chu <technicat@users.noreply.github.com>
2024-04-21 18:49:20 -0600

    add TootClient.getNotifications
    with/without parameters instead of param struct

---
+++ Sources/Fedicat/Extensions/TootSDK/TootClient/TootClientNotificationGet.swift
@@ -0,0 +1,18 @@
+import TootSDK
+
+extension TootClient {
+    
+    public func getNotifications(
+        with types: Notifications? = nil,
+        without exclude: Notifications? = nil,
+        _ page: PagedInfo? = nil,
+        limit: Int? = nil
+    ) async throws -> PagedResult<[TootNotification]> {
+        try await getNotifications(
+            params:
+                TootNotificationParams(excludeTypes: exclude,
+                                       types: types),
+            page,
+            limit: limit)
+    }
+}
