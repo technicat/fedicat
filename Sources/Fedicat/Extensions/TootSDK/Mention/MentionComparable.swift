9372584ec63b78f92924571610e265ff5bb62a49
Philip Chu <technicat@users.noreply.github.com>
2024-04-25 16:23:23 -0600

    Update MentionComparable.swift

--- Sources/Fedicat/Extensions/TootSDK/Mention/MentionComparable.swift
+++ Sources/Fedicat/Extensions/TootSDK/Mention/MentionComparable.swift
@@ -1,5 +1,6 @@
 import TootSDK
 
+// should this be case-insensitive?
 extension Mention: Comparable {
   public static func < (lhs: TootSDK.Mention, rhs: TootSDK.Mention) -> Bool {
     lhs.username < rhs.username
