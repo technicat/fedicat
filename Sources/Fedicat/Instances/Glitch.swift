4a620efcc85710e12c5f225d3daedbf4ba05fe18
Philip Chu <technicat@users.noreply.github.com>
2024-04-28 12:53:46 -0600

    masto glitch migration

--- Sources/Fedicat/Instances/Glitch.swift
+++ Sources/Fedicat/Instances/Glitch.swift
@@ -2,7 +2,13 @@ public let glitch = [
   "computerfairi.es",
   "cupoftea.social",
   "eldritch.cafe",
+  "emacs.ch",
+  "expressional.social",
   "functional.cafe",
+  "infosec.exchange",
+  "infosec.space",
+  "indiepocalypse.social",
   "inherently.digital",
+  "mastodon.de",
   "tech.lgbt",
 ]
