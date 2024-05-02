031f739bc43a68187c6fa67ab7c7aa26dd39f1d4
Philip Chu <technicat@users.noreply.github.com>
2024-05-02 17:27:15 -0600

    Create SessionBoost.swift

---
+++ Sources/Fedicat/Session/SessionBoost.swift
@@ -0,0 +1,24 @@
+import TootSDK
+
+extension Session {
+
+  /// some platforms don't properly set isBoosted/isReblogged
+  /// used to be all the Misskey forks, not just Sharkey
+  public var supportsIsBoosted: Bool {
+    !isSharkey
+  }
+
+  public func boost(_ post: Post) async throws -> Post {
+    guard !post.isBoosted else {
+      return post
+    }
+    return try await client.boost(post)
+  }
+
+  public func unBoost(_ post: Post) async throws -> Post {
+    guard post.isBoosted else {
+      return post
+    }
+    return try await client.unBoost(post)
+  }
+}
