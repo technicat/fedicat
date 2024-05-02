7add2a781ef6a8594687b2b85cd582171c5408ba
Philip Chu <technicat@users.noreply.github.com>
2024-05-02 17:05:11 -0600

    Session.isSharkey

--- Sources/Fedicat/Session/SessionPlatform.swift
+++ Sources/Fedicat/Session/SessionPlatform.swift
@@ -1,10 +1,14 @@
 import TootSDK
 
-// kludge for platform special cases
+// to support platform special case kludges
 // when we don't want to bother putting them in Platform
 extension Session {
 
   public var isPixelfed: Bool {
     platform is Pixelfed
+  }
+
+  public var isSharkey: Bool {
+    platform is Sharkey
   }
 }
