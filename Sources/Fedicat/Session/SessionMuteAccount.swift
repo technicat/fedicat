ec4af66a7c776b381562a9cf125dd92a5dd34f89
Philip Chu <technicat@users.noreply.github.com>
2024-05-02 12:35:36 -0600

    Create SessionMuteAccount.swift

---
+++ Sources/Fedicat/Session/SessionMuteAccount.swift
@@ -0,0 +1,36 @@
+import TootSDK
+
+extension Session {
+
+  public func mute(
+    _ account: Account,
+    notifications: Bool = true,
+    duration: Int = 0
+  ) async throws -> Relationship {
+    return try await client.mute(
+      account,
+      notifications: notifications,
+      duration: duration)
+  }
+
+  public func unMute(account: Account) async throws -> Relationship {
+    try await client.unMute(account)
+  }
+
+  public func getMutedAccounts(_ page: PagedInfo? = nil) async throws
+    -> PagedResult<[Account]>
+  {
+    let result = try await client.getMutedAccounts(
+      page,
+      limit: orNil(mutedAccountsLimit, 40))
+    await addAccounts(result.result)
+    return result
+  }
+
+  private var mutedAccountsLimit: Int {
+      // this was originally here because pixelfed had a max of 40
+      // use max limit to minimize queries
+      // we could get this from Platform
+      return 80
+  }
+}
