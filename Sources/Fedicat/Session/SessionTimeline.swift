91401d86aecab6f5f3ff5e465af468764220fbbd
Philip Chu <technicat@users.noreply.github.com>
2024-05-03 17:15:59 -0600

    Create SessionTimeline.swift

---
+++ Sources/Fedicat/Session/SessionTimeline.swift
@@ -0,0 +1,38 @@
+import TootSDK
+
+extension Session {
+
+  @MainActor public func setTimeline(_ timeline: Timeline) {
+    self.timeline = timeline
+  }
+
+  public func getTimeline(
+    _ timeline: Timeline,
+    _ page: PagedInfo? = nil
+  ) async throws -> PagedResult<[Post]> {
+    let result = try await client.getTimeline(
+      timeline,
+      pageInfo: page,
+      limit: getTimelineLimit(timeline))
+    let posts = result.result
+    await addPostAccounts(posts)
+    return result
+  }
+
+    // todo - get this from platform
+  private func getTimelineLimit(_ timeline: Timeline) -> Int {
+    switch timeline {
+    default:
+      // use max limit to minimize queries
+      return 40
+    }
+  }
+
+  public var supportsFaveTimeline: Bool {
+    platform?.supportsFaveTimeline ?? false
+  }
+
+  public var supportsTagTimeline: Bool {
+    platform?.supportsTagTimeline ?? false
+  }
+}
