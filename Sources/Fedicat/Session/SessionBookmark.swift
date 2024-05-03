33561f30ee0a353da91498214400e32e10ef53d7
Philip Chu <technicat@users.noreply.github.com>
2024-05-02 19:27:26 -0600

    Create SessionBookmark.swift

---
+++ Sources/Fedicat/Session/SessionBookmark.swift
@@ -0,0 +1,28 @@
+import TootSDK
+
+extension Session {
+
+  public var canBookmark: Bool {
+    isAuth && supportsBookmark
+  }
+
+  public var supportsBookmark: Bool {
+    platform?.supportsBookmark ?? false
+  }
+
+  @discardableResult
+  public func bookmark(_ post: Post) async throws -> Post {
+    guard !post.isBooked else {
+      return post
+    }
+    return try await client.bookmark(post)
+  }
+
+  @discardableResult
+  public func unBookmark(_ post: Post) async throws -> Post {
+    guard post.isBooked else {
+      return post
+    }
+    return try await client.unBookmark(post)
+  }
+}
