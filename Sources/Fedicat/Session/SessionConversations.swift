ef965def0fd93ca5a8867a3b245a8ccadcd124ce
Philip Chu <technicat@users.noreply.github.com>
2024-05-03 12:07:06 -0600

    Create SessionConversations.swift

---
+++ Sources/Fedicat/Session/SessionConversations.swift
@@ -0,0 +1,30 @@
+import TootSDK
+
+extension Session {
+
+  public func getConversations(_ page: PagedInfo? = nil) async throws
+    -> PagedResult<[Conversation]>
+  {
+    let result = try await client.getConversations(
+      page,
+      limit: conversationsPageLimit)
+    for convo in result.result {
+      await addAccounts(convo.accounts)
+    }
+    return result
+  }
+
+  // todo - get from Platform
+  public var conversationsPageLimit: Int {
+    40
+  }
+
+  public func delete(_ conversation: Conversation) async throws {
+    try await client.delete(conversation)
+  }
+
+  @discardableResult
+  public func mark(_ conversation: Conversation) async throws -> Conversation {
+    try await client.mark(conversation)
+  }
+}
