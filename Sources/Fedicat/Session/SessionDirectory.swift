7dae69c5d9dc8c449fc1053c046f7621ece624c0
Philip Chu <technicat@users.noreply.github.com>
2024-05-03 14:04:09 -0600

    Create SessionDirectory.swift

---
+++ Sources/Fedicat/Session/SessionDirectory.swift
@@ -0,0 +1,36 @@
+import TootSDK
+
+extension Session {
+
+  public var supportsDirectory: Bool {
+    platform?.supportsProfileDirectory ?? false
+  }
+
+  public func getLocalDirectory(
+    offset: Int? = nil,
+    order: ProfileDirectoryParams.Order? = nil
+  ) async throws -> [Account] {
+    let accounts = try await client.getLocalDirectory(
+      order: order,
+      offset: offset,
+      limit: directoryLimit
+    )
+    await addAccounts(accounts)
+    return accounts
+  }
+
+  public func getGlobalDirectory(
+    offset: Int? = nil,
+    order: ProfileDirectoryParams.Order? = nil
+  ) async throws -> [Account] {
+    let accounts = try await client.getGlobalDirectory(
+      order: order,
+      offset: offset,
+      limit: directoryLimit
+    )
+    await addAccounts(accounts)
+    return accounts
+  }
+
+    private var directoryLimit: Int { 80 } // todo - get from platform
+}
