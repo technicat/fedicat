39a07c287aac443ea79e498ed371847f3808bda7
Philip Chu <technicat@users.noreply.github.com>
2024-05-03 19:07:01 -0600

    Create SessionMutedAccounts.swift

---
+++ Sources/Fedicat/Session/SessionMutedAccounts.swift
@@ -0,0 +1,18 @@
+import TootSDK
+
+extension Session {
+
+  public func getMutedAccounts(_ page: PagedInfo? = nil) async throws
+    -> PagedResult<[Account]>
+  {
+    let result = try await client.getMutedAccounts(
+      page,
+      limit: mutedAccountsLimit)
+    await addAccounts(result.result)
+    return result
+  }
+
+  private var mutedAccountsLimit: Int {
+    platform?.mutedAccountsLimit ?? 80
+  }
+}
