0502d3b560f0848497fae745114be5ada3763568
Philip Chu <technicat@users.noreply.github.com>
2024-05-03 18:54:36 -0600

    Create SessionNotifications.swift

---
+++ Sources/Fedicat/Session/SessionNotifications.swift
@@ -0,0 +1,45 @@
+import TootSDK
+
+extension Session {
+
+  public var supportsNotificationTypes: Bool {
+    platform?.hasNotificationTypes ?? false
+  }
+
+  public var notificationTypes: NotificationTypes {
+    platform?.notificationTypes ?? []
+  }
+
+  public func supportsNotificationTypes(_ types: NotificationTypes) -> NotificationTypes {
+    notificationTypes.intersection(types)
+  }
+
+  public func notNotificationTypes(_ types: NotificationTypes) -> NotificationTypes {
+    notificationTypes.subtracting(types)
+  }
+
+  public func getNotifications(
+    with types: NotificationTypes,
+    _ page: PagedInfo? = nil
+  ) async throws -> PagedResult<[TootNotification]> {
+    // friendica requires exclusion types
+    // or you just end up getting everything
+    // doesn't seem to hurt elsewhere
+    let result = try await client.getNotifications(
+      with: types, without: notNotificationTypes(types),
+      page,
+      limit: notificationPageLimit)
+    let notes = result.result
+    // assume no boosts
+    for note in notes {
+      await addAccount(note)
+      if let post = note.post {
+        await addAccount(post)
+      }
+    }
+    return result
+  }
+    
+    private var notificationPageLimit: Int { 30 } // todo - platform
+
+}
