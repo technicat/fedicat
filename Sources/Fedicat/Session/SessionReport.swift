b600f127f3a3cc294e37aeb1562505f149475425
Philip Chu <technicat@users.noreply.github.com>
2024-05-04 11:14:22 -0600

    Create SessionReport.swift

---
+++ Sources/Fedicat/Session/SessionReport.swift
@@ -0,0 +1,29 @@
+import TootSDK
+
+extension Session {
+
+  public var supportsReportRules: Bool {
+    platform?.supportsReportRules ?? false
+  }
+
+  public var reportCategories: [ReportCategory] {
+    platform?.reportCats ?? []
+  }
+
+  public func report(
+    account: Account,
+    comment: String,
+    forward: Bool = false,
+    category: ReportCategory,
+    posts: [Post],
+    rules: [InstanceRule] = []
+  ) async throws {
+    try await client.report(
+      account: account,
+      comment: comment.orNil,
+      forward: orNil(forward, false),
+      category: category,
+      posts: posts,
+      rules: rules)
+  }
+}
