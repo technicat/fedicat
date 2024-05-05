609be36ff15725582191d0e517f73bfa2229f100
Philip Chu <technicat@users.noreply.github.com>
2024-05-05 11:38:55 -0600

    Create SessionAccountUpdate.swift

---
+++ Sources/Fedicat/Session/SessionAccountUpdate.swift
@@ -0,0 +1,68 @@
+import TootSDK
+
+extension Session {
+
+  public var supportsUpdateAccount: Bool {
+    platform?.supportsUpdateAccount ?? false
+  }
+
+  public func update(_ settings: DraftAccountSettings) async throws -> Account {
+    let account = try await client.updateAccountSettings(
+      locked: settings.locked,
+      bot: settings.bot,
+      discoverable: settings.discoverable,
+      hideCollections: settings.hideCollections,
+      indexable: settings.indexable)
+    await setAccount(account)
+    return account
+  }
+
+  public func update(_ profile: DraftProfile) async throws -> Account {
+    let account = try await client.updateProfile(
+      displayName: profile.displayName,
+      note: profile.note,
+      avatar: profile.avatar?.jpeg(),
+      avatarMimeType: .jpeg,
+      header: profile.header?.jpeg(),
+      headerMimeType: .jpeg
+    )
+    await setAccount(account)
+    return account
+  }
+
+  public func updateFields(_ profile: DraftProfile) async throws -> Account {
+    var map = [String: UpdateCredentialsParams.Field]()
+    for index in 0..<profile.fields.filter({ !$0.isEmpty }).count {
+      map[String(index)] = UpdateCredentialsParams.Field(
+        name: profile.fields[index].name.trimWS,
+        value: profile.fields[index].value.trimWS)
+    }
+    let account = try await client.updateProfile(
+      fields: map.isEmpty ? nil : map)
+    await setAccount(account)
+    return account
+  }
+
+  public func update(_ defs: DraftPostDefaults) async throws -> Account {
+    let account = try await client.updatePostDefaults(
+      privacy: defs.visibility,
+      sensitive: defs.sensitive,
+      language: defs.lang)
+    await setAccount(account)
+    return account
+  }
+
+  @discardableResult
+  public func deleteAvatar() async throws -> Account {
+    let account = try await client.deleteAvatar()
+    await setAccount(account)
+    return account
+  }
+
+  @discardableResult
+  public func deleteHeader() async throws -> Account {
+    let account = try await client.deleteHeader()
+    await setAccount(account)
+    return account
+  }
+}
