18186d67bc9e989821198fccf73a43338d922102
Philip Chu <technicat@users.noreply.github.com>
2024-05-04 23:12:35 -0600

    Create SessionList.swift

---
+++ Sources/Fedicat/Session/SessionList.swift
@@ -0,0 +1,43 @@
+import Foundation
+import TootSDK
+
+extension Session {
+
+  public var supportsList: Bool {
+    isAuth && (platform?.supportsList ?? false)
+  }
+
+  public var supportsListReplyPolicy: Bool {
+    platform?.supportsListReplyPolicy ?? false
+  }
+
+  public var supportsListExclusive: Bool {
+    platform?.supportsListExclusive ?? false
+  }
+
+  @MainActor public func add(_ list: List) {
+    lists[list.id] = list
+  }
+
+  @MainActor public func remove(_ list: List) {
+    lists[list.id] = nil
+  }
+
+  @ObservationIgnored
+  public var hasList: Bool {
+    lists.isEmpty
+  }
+
+  private var listsArray: [List] {
+    lists.map(\.1)
+  }
+
+  @ObservationIgnored
+  public var listsSorted: [List] {
+    listsArray.sorted(by: <)
+  }
+
+  public func lists(minus list: [List]) -> [List] {
+    listsArray.filter { !list.contains($0) }
+  }
+}
