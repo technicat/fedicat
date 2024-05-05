ed0501752e40aa53a9f329c5fc9abc31aeab643e
Philip Chu <technicat@users.noreply.github.com>
2024-05-04 23:06:46 -0600

    add getEndorsements

--- Sources/Fedicat/Session/SessionEndorse.swift
+++ Sources/Fedicat/Session/SessionEndorse.swift
@@ -1,6 +1,7 @@
 import TootSDK
 
-// doesn't seem like it does anything now (no way to display endorsed accounts)
+// doesn't seem like it does anything now
+// (no way to display endorsed accounts from another account)
 // but keep it around until it's officially deprecated?
 extension Session {
 
@@ -8,11 +9,25 @@ extension Session {
     false
   }
 
+  @discardableResult
   public func endorse(_ account: Account) async throws -> Relationship {
     try await client.endorse(account)
   }
 
+  @discardableResult
   public func unEndorse(_ account: Account) async throws -> Relationship {
     try await client.unEndorse(account)
   }
+
+  public func getEndorsements(_ page: PagedInfo? = nil) async throws
+    -> PagedResult<[Account]>
+  {
+    let result = try await client.getEndorsements(
+      page,
+      limit: endorsementsLimit)
+    await addAccounts(result.result)
+    return result
+  }
+
+  private var endorsementsLimit: Int { 80 }  // platform
 }
