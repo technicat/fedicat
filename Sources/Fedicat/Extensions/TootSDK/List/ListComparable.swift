57c4e8ec3c50751744cf22dd31912534a9c97ec2
Philip Chu <technicat@users.noreply.github.com>
2024-05-04 23:39:07 -0600

    format

--- Sources/Fedicat/Extensions/TootSDK/List/ListComparable.swift
+++ Sources/Fedicat/Extensions/TootSDK/List/ListComparable.swift
@@ -4,6 +4,6 @@ import TootSDK
 extension TootSDK.List: Comparable {
 
   public static func < (lhs: TootSDK.List, rhs: TootSDK.List) -> Bool {
-      lhs.title.caseInsensitiveCompare(rhs.title) == ComparisonResult.orderedAscending
+    lhs.title.caseInsensitiveCompare(rhs.title) == ComparisonResult.orderedAscending
   }
 }
