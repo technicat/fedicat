baeadd8aeeabd7efbfb03bba6f1157eb76acef7d
Philip Chu <technicat@users.noreply.github.com>
2024-05-06 10:31:38 -0600

    Create SessionTagFollow.swift

---
+++ Sources/Fedicat/Session/SessionTagFollow.swift
@@ -0,0 +1,70 @@
+import TootSDK
+
+extension Session {
+
+  public var canFollowTag: Bool {
+    isAuth && supportsTagFollow
+  }
+    
+    public var supportsTagFollow: Bool {
+      platform?.supportsFollowTag ?? false
+    }
+
+  @MainActor func add(follow tag: Tag) {
+    followedTags[tag.name] = tag
+  }
+
+  @MainActor func remove(follow tag: Tag) {
+    followedTags[tag.name] = nil
+  }
+
+  // maybe MainActor?
+  public var followedTagsArray: [Tag] {
+    followedTags.map(\.1)
+  }
+
+  //  @ObservationIgnored
+  //  var followedTagsSorted: [Tag] {
+  //    followedTagsArray.sorted(by: <)
+  //  }
+
+  @discardableResult
+  public func follow(tag name: String) async throws -> Tag {
+    let tag = try await client.follow(tag: name)
+    await add(follow: tag)
+    return tag
+  }
+
+  public func follow(tag: Tag) async throws -> Tag {
+    try await follow(tag: tag.name)
+  }
+
+  public func unFollow(tag name: String) async throws -> Tag {
+    let tag = try await client.unFollow(tag: name)
+    await remove(follow: tag)
+    return tag
+  }
+
+  public func unFollow(tag: Tag) async throws -> Tag {
+    try await unFollow(tag: tag.name)
+  }
+
+  /// https://docs.joinmastodon.org/methods/followed_tags/
+  public func getFollowedTags(_ page: PagedInfo? = nil) async throws -> PagedResult<
+    [Tag]
+  > {
+    let result = try await client.getFollowedTags(page, limit: followedTagsPageLimit)
+    for tag in result.result {
+      await add(follow: tag)
+    }
+    return result
+  }
+    
+    private var followedTagsPageLimit: Int? {
+        200
+    }
+
+  public func getAllFollowedTags() async throws {
+    try await client.getPages { page in try await getFollowedTags(page) }
+  }
+}
