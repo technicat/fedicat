3114a45a58223622293d29c06d9446ffd53316f9
Philip Chu <technicat@users.noreply.github.com>
2024-05-05 20:11:33 -0600

    more kludges

--- Sources/Fedicat/Session/SessionPostRefresh.swift
+++ Sources/Fedicat/Session/SessionPostRefresh.swift
@@ -5,8 +5,18 @@ extension Session {
   // pixelfed bug - nulls reblogged/favourited on like/boost
   public var requiresRefreshAfterBoostLike: Bool {
     // todo - maybe move this to Platform
-    platform is Pixelfed
+      requiresRefreshAfterBoost || requiresRefreshAfterBoost
   }
+    
+    public var requiresRefreshAfterBoost: Bool {
+      // todo - maybe move this to Platform
+      platform is Pixelfed || platform is Mitra
+    }
+    
+    public var requiresRefreshAfterLike: Bool {
+      // todo - maybe move this to Platform
+      platform is Pixelfed
+    }
 
   public func refresh(_ post: Post) async throws -> Post {
     let post = try await client.refresh(post)
