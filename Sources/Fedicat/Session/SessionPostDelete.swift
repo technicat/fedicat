429c061071963b04ac1719ac25558eb045e490cc
Philip Chu <technicat@users.noreply.github.com>
2024-05-07 12:05:07 -0600

    call version of delete post with no return value

--- Sources/Fedicat/Session/SessionPostDelete.swift
+++ Sources/Fedicat/Session/SessionPostDelete.swift
@@ -4,10 +4,20 @@ extension Session {
 
   @discardableResult
   public func delete(_ post: Post) async throws -> Post {
-    let post = try await client.delete(post)
-    // ideally the post should disappear from view
-    // but if not, show that it's really deleted
-    post.content = "*deleted*"  // localize
-    return post
+    if deletePostReturnsPost {
+      let post = try await client.delete(post)
+      // ideally the post should disappear from view
+      // but if not, show that it's really deleted
+      post.content = "*deleted*"  // localize
+      return post
+    } else {
+      try await client.delete(withoutReturn: post)
+      post.content = "*deleted*"  // localize
+      return post
+    }
+  }
+
+  private var deletePostReturnsPost: Bool {
+    false  // todo - move to platform
   }
 }
