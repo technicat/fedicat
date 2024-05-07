015fddbdbdda83f4396cf078b14d359f2940b5f3
Philip Chu <technicat@users.noreply.github.com>
2024-05-06 23:31:04 -0600

    format

--- Sources/Fedicat/Session/SessionBoost.swift
+++ Sources/Fedicat/Session/SessionBoost.swift
@@ -10,17 +10,17 @@ extension Session {
   @discardableResult
   public func boost(_ post: Post) async throws -> Post {
     guard !post.isBoosted else {
-        // assume we don't allow/want multiple boosts
-        // even though some (like Sharkey) seem to allow it
-        // probably inadvertently
-        return post 
+      // assume we don't allow/want multiple boosts
+      // even though some (like Sharkey) seem to allow it
+      // probably inadvertently
+      return post
     }
     let post = try await client.boost(post)
-      // mastodon spec says the boosted post is in the reblog field
+    // mastodon spec says the boosted post is in the reblog field
     // of the boosting post which is returned
-      // but some like Pixelfed return the boosted post
-      let boosted = post.displayPost
-      return boosted
+    // but some like Pixelfed return the boosted post
+    let boosted = post.displayPost
+    return boosted
   }
 
   @discardableResult
--- Sources/Fedicat/Session/SessionError.swift
+++ Sources/Fedicat/Session/SessionError.swift
@@ -1,9 +1,9 @@
 import Foundation
 import TootSDK
 
-    public enum SessionError: Error, LocalizedError, Equatable {
-        case noDomain // 
-        case noSecret // 
-        case noReblog // should pass post
-        case alreadyBoosted //
+public enum SessionError: Error, LocalizedError, Equatable {
+  case noDomain  //
+  case noSecret  //
+  case noReblog  // should pass post
+  case alreadyBoosted  //
 }
