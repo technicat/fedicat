348ca75676e70175da517c45f46a49c937ce091f
Philip Chu <technicat@users.noreply.github.com>
2024-05-07 21:57:23 -0600

    rename delete post methods

--- Sources/Fedicat/Session/SessionPostDelete.swift
+++ Sources/Fedicat/Session/SessionPostDelete.swift
@@ -4,20 +4,20 @@ extension Session {
 
   @discardableResult
   public func delete(_ post: Post) async throws -> Post {
-    if deletePostReturnsPost {
-      let post = try await client.delete(post)
+    if supportsDeleteAndEditPost {
+      let post = try await client.deleteAndEdit(post)
       // ideally the post should disappear from view
       // but if not, show that it's really deleted
       post.content = "*deleted*"  // localize
       return post
     } else {
-      try await client.delete(withoutReturn: post)
+      try await client.delete(post)
       post.content = "*deleted*"  // localize
       return post
     }
   }
 
-  private var deletePostReturnsPost: Bool {
+  private var supportsDeleteAndEditPost: Bool {
     false  // todo - move to platform
   }
 }
