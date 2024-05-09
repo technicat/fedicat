57c77556164641d2188dd0b088318365637afa3c
Philip Chu <technicat@users.noreply.github.com>
2024-05-08 22:35:17 -0600

    Create SessionSearchAccount.swift

---
+++ Sources/Fedicat/Session/SessionSearchAccount.swift
@@ -0,0 +1,20 @@
+import TootSDK
+
+/// https://docs.joinmastodon.org/methods/search/
+extension Session {
+
+  public func search(
+    in account: Account,
+    _ query: String
+  ) async throws -> Search {
+    let result = try await client.search(
+      params: SearchParams(
+        query: query.trimWSNL,
+        type: .posts,
+        accountId: account.id))
+    await addAccounts(result.accounts)
+    await addAccounts(result.posts)
+    // maybe cache tags
+    return result
+  }
+}
