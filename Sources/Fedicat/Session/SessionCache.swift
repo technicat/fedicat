92f69709526f30627e6f0cfb0992017cf5f5bc00
Philip Chu <technicat@users.noreply.github.com>
2024-05-09 22:59:43 -0600

    Create SessionCache.swift

---
+++ Sources/Fedicat/Session/SessionCache.swift
@@ -0,0 +1,43 @@
+import TootSDK
+
+extension Session {
+
+  public func cache(_ priority: TaskPriority) {
+    //  let priority = Session.backPriority
+    // split these into functions
+    if supportsTagFollow {
+      Task(priority: priority) {
+        try await getAllFollowedTags()
+      }
+    }
+    if supportsFeaturedTags {
+      Task(priority: priority) {
+        try await getFeaturedTags()
+      }
+      Task(priority: priority) {
+        try await getFeaturedTagsSuggestions()
+      }
+    }
+    // so we can have a list menu
+    if supportsList {
+      Task(priority: priority) {
+        try await getLists()
+      }
+    }
+    if canReadAnnouncements {
+      Task(priority: priority) {
+        try await getAnnouncements()
+      }
+    }
+    if supportsSchedule {
+      Task(priority: priority) {
+        try await getScheduledPosts()
+      }
+    }
+    if supportsTranslationLanguages {
+      Task(priority: priority) {
+        try await getTranslationLanguages()
+      }
+    }
+  }
+}
