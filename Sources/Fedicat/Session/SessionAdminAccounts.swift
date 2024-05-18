182d29e10cd6460ed88dec05245feb86152a547f
Philip Chu <technicat@users.noreply.github.com>
2024-05-17 20:55:06 -0600

    Create SessionAdminAccounts.swift

---
+++ Sources/Fedicat/Session/SessionAdminAccounts.swift
@@ -0,0 +1,31 @@
+import TootSDK
+
+extension Session {
+
+  public func getAccountsV1(
+    _ account: Account,
+    _ page: PagedInfo? = nil
+  ) async throws -> PagedResult<[Account]> {
+    let result = try await client.getAccountsV1(
+      page,
+      limit: accountsPageLimit)
+    await addAccounts(result.result)
+    return result
+  }
+
+  public func getAccountsV2(
+    _ account: Account,
+    _ page: PagedInfo? = nil
+  ) async throws -> PagedResult<[Account]> {
+    let result = try await client.getAccountsV2(
+      page,
+      limit: accountsPageLimit)
+    await addAccounts(result.result)
+    return result
+  }
+
+  private var accountsPageLimit: Int {
+    40
+    // platform?.followersPageLimit ?? 0
+  }
+}
