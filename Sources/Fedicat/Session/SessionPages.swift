11768c05320c51ad65913eac62ae9c8b48c41f85
Philip Chu <technicat@users.noreply.github.com>
2024-05-20 11:14:13 -0600

    make getPages internal

--- Sources/Fedicat/Session/SessionPages.swift
+++ Sources/Fedicat/Session/SessionPages.swift
@@ -4,7 +4,7 @@ extension Session {
 
     /// convenience function to fetch multiple pages
     /// default maxPages is arbitrary, but we don't want go nuts
-    public func getPages<T>(maxPages: Int = 5, query: (PagedInfo?) async throws -> PagedResult<T>)
+    func getPages<T>(maxPages: Int = 5, query: (PagedInfo?) async throws -> PagedResult<T>)
         async throws
     {
         var count = 0
