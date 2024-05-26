96ea02ea2293ee7ebc76c46d4d2df0cdc20ab70d
Philip Chu <technicat@users.noreply.github.com>
2024-05-25 19:44:54 -0600

    rename getAdminAccounts

--- Sources/Fedicat/Session/SessionAdminAccounts.swift
+++ Sources/Fedicat/Session/SessionAdminAccounts.swift
@@ -2,20 +2,10 @@ import TootSDK
 
 extension Session {
 
-  public func getAccountsV1(
+  public func getAdminAccounts(
     _ page: PagedInfo? = nil
   ) async throws -> PagedResult<[AdminAccount]> {
-    let result = try await client.getAccountsV1(
-      page,
-      limit: adminAccountsPageLimit)
-    await addAccounts(result.result)
-    return result
-  }
-
-  public func getAccountsV2(
-    _ page: PagedInfo? = nil
-  ) async throws -> PagedResult<[AdminAccount]> {
-    let result = try await client.getAccountsV2(
+    let result = try await client.getAdminAccountsV2(
       page,
       limit: adminAccountsPageLimit)
     await addAccounts(result.result)
