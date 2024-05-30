a4a7ccfefdc14552bb419498fac109950449b6ac
Philip Chu <technicat@users.noreply.github.com>
2024-05-29 20:58:13 -0600

    moved session init back to app level

--- Sources/Fedicat/Session/SessionInit.swift
+++ Sources/Fedicat/Session/SessionInit.swift
@@ -1,22 +1,23 @@
-extension Session {
-
-  // should handle errors
-  public func initialize(_ priority: TaskPriority) {
-    if isAuth {
-      Task(priority: priority) {
-        // should use this to verify token?
-        // need this for username display
-        // and for finding followers
-        try await getAccount()
-        try await getAllFollowedAccounts()
-      }
-    }
-    Task(priority: TaskPriority.high) {
-      try await getInstance()  // sets flavor
-      if isAuth {
-        cache(priority)
-      }
-    }
-
-  }
-}
+//extension Session {
+//
+//  // should handle errors
+//  public func initialize(_ priority: TaskPriority) {
+//    if isAuth {
+//      Task(priority: priority) {
+//        // should use this to verify token?
+//        // need this for username display
+//        // and for finding followers
+//        try await getAccount()
+//        try await getAllFollowedAccounts()
+//      }
+//    }
+//    Task(priority: TaskPriority.high) {
+//        try await getNodeInfo(at: domain)
+//      try await getInstance()  // sets flavor
+//      if isAuth {
+//        cache(priority)
+//      }
+//    }
+//
+//  }
+//}
