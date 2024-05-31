279a3bfb58f0e49e0e3d806f469c7292ae989133
Philip Chu <technicat@users.noreply.github.com>
2024-05-31 13:08:20 -0600

    return nil if timeline not supported

--- Sources/Fedicat/Session/SessionTimeline.swift
+++ Sources/Fedicat/Session/SessionTimeline.swift
@@ -9,14 +9,18 @@ extension Session {
   public func getTimeline(
     _ timeline: Timeline,
     _ page: PagedInfo? = nil
-  ) async throws -> PagedResult<[Post]> {
-    let result = try await client.getTimeline(
-      timeline,
-      pageInfo: page,
-      limit: getPageLimit(for: timeline))
-    let posts = result.result
-    await addPostAccounts(posts)
-    return result
+  ) async throws -> PagedResult<[Post]>? {
+      if supports(timeline) {
+          let result = try await client.getTimeline(
+            timeline,
+            pageInfo: page,
+            limit: getPageLimit(for: timeline))
+          let posts = result.result
+          await addPostAccounts(posts)
+          return result
+      } else {
+          return nil
+      }
   }
 
   private func getPageLimit(for timeline: Timeline) -> Int {
