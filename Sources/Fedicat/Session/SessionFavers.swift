9a4eea70d31411728539630db5923d9a23876ee6
Philip Chu <technicat@users.noreply.github.com>
2024-05-31 13:57:48 -0600

    query guards

--- Sources/Fedicat/Session/SessionFavers.swift
+++ Sources/Fedicat/Session/SessionFavers.swift
@@ -6,10 +6,9 @@ extension Session {
     faversPageLimit > 0
   }
 
-  public func getFavers(
-    of post: Post,
-    _ page: PagedInfo? = nil
-  ) async throws -> PagedResult<[Account]> {
+  public func getFavers(of post: Post, _ page: PagedInfo? = nil
+  ) async throws -> PagedResult<[Account]>? {
+      guard supportsFavers else { return nil }
     let result = try await client.getFavers(
       of: post,
       page,
--- Sources/Fedicat/Session/SessionFollowers.swift
+++ Sources/Fedicat/Session/SessionFollowers.swift
@@ -2,10 +2,9 @@ import TootSDK
 
 extension Session {
 
-  public func getFollowers(
-    _ account: Account,
-    _ page: PagedInfo? = nil
-  ) async throws -> PagedResult<[Account]> {
+  public func getFollowers(_ account: Account, _ page: PagedInfo? = nil
+  ) async throws -> PagedResult<[Account]>? {
+      guard supportsFollowers else { return nil }
     let result = try await client.getFollowers(
       of: account,
       page,
@@ -13,6 +12,10 @@ extension Session {
     await addAccounts(result.result)
     return result
   }
+    
+    public var supportsFollowers: Bool {
+        followersPageLimit > 0
+    }
 
   private var followersPageLimit: Int {
     platform?.followersPageLimit ?? 0
