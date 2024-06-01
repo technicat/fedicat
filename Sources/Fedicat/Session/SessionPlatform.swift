4055b1991c692b5a7f18d96b3cc4a54ce5d3edd2
Philip Chu <technicat@users.noreply.github.com>
2024-05-31 18:43:46 -0600

    format

--- Sources/Fedicat/Session/SessionPlatform.swift
+++ Sources/Fedicat/Session/SessionPlatform.swift
@@ -1,10 +1,10 @@
 import TootSDK
 
 extension Session {
-    
-    public var needsPlatformUpdate: Bool {
-        return platform?.needsUpdate ?? true
-    }
+
+  public var needsPlatformUpdate: Bool {
+    return platform?.needsUpdate ?? true
+  }
 
   @MainActor public func setPlatform(_ platform: Platform) {
     self.platform = platform
