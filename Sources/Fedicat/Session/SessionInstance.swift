18dbbe8e6489e2d0ec11075349bbe9d2ca6feab6
Philip Chu <technicat@users.noreply.github.com>
2024-05-31 18:33:42 -0600

    mv session platform stuff to SessionPlatform

--- Sources/Fedicat/Session/SessionInstance.swift
+++ Sources/Fedicat/Session/SessionInstance.swift
@@ -1,30 +1,17 @@
 import TootSDK
 
 extension Session {
-    
-    
-    public var needsPlatformUpdate: Bool {
-        guard let platform else { return true }
-        return platform.needsUpdate
-    }
 
-  @MainActor public func setInstanceV1(_ instance: Instance) {
+  @MainActor public func setInstance(_ instance: Instance) {
     self.instance = instance
-      if needsPlatformUpdate, let platform = instance.platform {
-          setPlatform(platform)
-      }
-  }
-
-  @MainActor public func setPlatform(_ platform: Platform) {
-    self.platform = platform
   }
 
   /// https://docs.joinmastodon.org/methods/instance/
   @discardableResult
   public func getInstance() async throws -> InstanceV1 {
     let instance = try await client.getInstance()
-    await setInstanceV1(instance)
-    if let platform = instance.platform {
+    await setInstance(instance)
+    if needsPlatformUpdate, let platform = instance.platform {
       await setPlatform(platform)
     }
     if let account = instance.contactAccount {
---
+++ Sources/Fedicat/Session/SessionPlatform.swift
@@ -0,0 +1,12 @@
+import TootSDK
+
+extension Session {
+    
+    public var needsPlatformUpdate: Bool {
+        return platform?.needsUpdate ?? true
+    }
+
+  @MainActor public func setPlatform(_ platform: Platform) {
+    self.platform = platform
+  }
+}
