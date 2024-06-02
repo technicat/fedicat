ee52bb451712538b6d849c27bed7c0821c951a1d
Philip Chu <technicat@users.noreply.github.com>
2024-06-02 14:16:51 -0600

    comment about getPeers

--- Sources/Fedicat/Session/SessionPeers.swift
+++ Sources/Fedicat/Session/SessionPeers.swift
@@ -2,6 +2,8 @@ import TootSDK
 
 extension Session {
 
+    /// works with short lists, but long lists (thousands) crash, so it's not really usable
+    /// https://docs.joinmastodon.org/methods/instance/#peers
   public func getPeers() async throws
     -> [String]
   {
