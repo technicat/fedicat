b4f21c3514c09a7dfb521b72da4c958bde2ca4ac
Philip Chu <technicat@users.noreply.github.com>
2024-06-04 00:04:52 -0600

    Update Akkoma.swift

--- Sources/Fedicat/Instances/Akkoma.swift
+++ Sources/Fedicat/Instances/Akkoma.swift
@@ -1,13 +1,17 @@
 public let akkoma = [
     "akkomane.social",
   "blob.cat",
+    "cdrom.tokyo",
   "fe.disroot.org",
   "genserver.social",
+    "labyrinth.zone",
+    "landofkittens.social",
   "miraiverse.xyz",
   "mycrowd.ca",
   "outerheaven.club",
   "pleroma.noellabo.jp",
-  // "seafoam.space",
+  "seafoam.space",
   "social.wedistribute.org",
+    "triangle.social",
   "turtleisland.rocks",
 ]
