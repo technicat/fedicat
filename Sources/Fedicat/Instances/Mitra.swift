d580adb38e1632b8f8af8eb6c760d55c8f4907fc
Philip Chu <technicat@users.noreply.github.com>
2024-06-04 18:33:41 -0600

    Update Mitra.swift

--- Sources/Fedicat/Instances/Mitra.swift
+++ Sources/Fedicat/Instances/Mitra.swift
@@ -1,9 +1,9 @@
 public let mitra = [
+    "fedi.libresolutions.network",
   "mitra.076.moe",
   "mitra.0ut0f.space",
   "mitra.anon-kenkai.com",
   "mitra.social",
   "public.mitra.social",
   "wizard.casa",
-
 ]
