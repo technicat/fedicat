6ecb780def4dca8ec308d3b8f89b176f7a0a941d
Philip Chu <technicat@users.noreply.github.com>
2024-06-09 10:51:51 -0600

    fix getBoosters paging

--- Sources/Fedicat/Session/SessionBoosters.swift
+++ Sources/Fedicat/Session/SessionBoosters.swift
@@ -8,9 +8,12 @@ extension Session {
 
   public func getBoosters(
     of post: Post,
-    _ page: PagedInfo? = nil
+    _ page: PagedInfo? = nil,
+    limit: Int? = nil
   ) async throws -> PagedResult<[Account]> {
-    let result = try await client.getBoosters(of: post)
+      let result = try await client.getBoosters(of: post, 
+                                                page, 
+                                                limit: limit)
     await addAccounts(result.result)
     return result
   }
