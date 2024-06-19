6a74b285205cda2e5f721fb00c5fa45cb2d8018a
Philip Chu <technicat@users.noreply.github.com>
2024-06-19 00:43:52 -0600

    Update SessionTranslation.swift

--- Sources/Fedicat/Session/SessionTranslation.swift
+++ Sources/Fedicat/Session/SessionTranslation.swift
@@ -37,14 +37,14 @@ extension Session {
   public func getTranslation(of post: Post, to language: ISOCode? = nil) async throws -> Post {
     if platform is Akkoma {
       let translation = try await client.getTranslationAkkoma(of: post, to: language ?? .en)
-      return getTranslation(of: post, from: translation)
+      return getTranslated(of: post, from: translation)
     } else {
       let translation = try await client.getTranslation(of: post, to: language)
-      return getTranslation(of: post, from: translation)
+      return getTranslated(of: post, from: translation)
     }
   }
 
-  public func getTranslation(of post: Post, from translation: Translated) -> Post {
+  public func getTranslated(of post: Post, from translation: Translated) -> Post {
     let newPost = Post(from: post)
     newPost.copy(from: translation)
     return newPost
@@ -55,6 +55,7 @@ extension Session {
   }
 }
 
+// need to localize
 extension Post {
 
   func copy(from translation: Translated) {
@@ -62,12 +63,16 @@ extension Post {
     if let source = translation.sourceLanguage?.localizedLanguageName {
       translated += source
     }
-    // todo - localize
     if let target = translation.targetLanguage?.localizedLanguageName {
-      translated += "to \(target)"
+      translated += " to \(target)"
     }
-    translated += " by \(translation.translator)</em></p>"
-    translated += "<p>\(translation.html)</p>"
+      if let translator = translation.translator {
+          translated += " by \(translator)"
+      }
+      translated += "</em></p>"
+      if let html = translation.html {
+          translated += "<p>\(html)</p>"
+      }
     content = translated
     if let spoiler = translation.spoiler {
       spoilerText = spoiler
