54a8def5ea40ce08947a2bba50a2931838259279
Philip Chu <technicat@users.noreply.github.com>
2024-06-30 16:09:30 -0600

    session limits

--- Sources/Fedicat/Session/SessionLimits.swift
+++ Sources/Fedicat/Session/SessionLimits.swift
@@ -17,12 +17,48 @@ extension Session {
   public var minAttachments: Int? {
     platform?.minAttachments
   }
+
+  public var charsPerURL: Int? {
+    instance?.charsPerURL
+  }
+
+  public var maxPins: Int? {
+    instance?.maxPins
+  }
     
-    public var charsPerURL: Int? {
-      instance?.charsPerURL
+    public var maxPollOptions: Int? {
+      instance?.maxPollOptions
     }
     
-    public var maxPins: Int? {
-      instance?.maxPins
+    public var maxPollChars: Int? {
+      instance?.maxPollChars
+    }
+    
+    public var minPollExpiration: Int? {
+      instance?.minPollExpiration
+    }
+    
+    public var maxPollExpiration: Int? {
+      instance?.maxPollExpiration
+    }
+    
+    public var imageSizeLimit: Int? {
+        instance?.configuration?.mediaAttachments?.imageSizeLimit
+    }
+    
+    public var imageMatrixLimit: Int? {
+        instance?.configuration?.mediaAttachments?.imageMatrixLimit
+    }
+    
+    public var videoSizeLimit: Int? {
+        instance?.configuration?.mediaAttachments?.videoSizeLimit
+    }
+    
+    public var videoMatrixLimit: Int? {
+        instance?.configuration?.mediaAttachments?.videoMatrixLimit
+    }
+    
+    public var videoFrameRateLimit: Int? {
+        instance?.configuration?.mediaAttachments?.videoFrameRateLimit
     }
 }
